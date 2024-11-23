bf6ca8eb9542c3c36a4a44eb0ccd4dbea808d169
Alexxtn105 <netmann105@gmail.com>
2024-11-23 21:07:28 +0300

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+# Тестовый проект с использованием Web-Sockets (пакет [Gorilla Web Socket](https://github.com/gorilla/websocket))
+
+Установка пакета Gorilla Web Socket:
+```bash
+go get github.com/gorilla/websocket 
+```
+
+Запуск сервиса:
+```bash
+go run .
+```
+
+Запуск клиента - открыть файл [client.html](client.html) в браузере
+
+Отправить сообщение - оно должно вернуться
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+// upgrader - главная задача - преобразование (апгрейд) http-соединения в WebSocket-соединение
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024, // размер буфера чтения
+	WriteBufferSize: 1024, // размер буфера записи
+	CheckOrigin: func(r *http.Request) bool { // разрешаем доступ всем
+		return true
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// Апгрейдим http-соединение до WebSocket-соединения
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//обязательно закрываем соединение по завершении работы
+	defer conn.Close()
+
+	// обработка входящих сообщений (используя метод readMessage соединения)
+	for {
+		messageType, data, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//выводим принятое сообщение
+		fmt.Printf("Received message: %s\n", data)
+
+		// отправляем клиенту то же самое сообщение обратно
+		if err := conn.WriteMessage(messageType, data); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+}
+
+func main() {
+	//fmt.Println("Hello, World!")
+	http.HandleFunc("/ws", handleWebSocket)
+	addr := "localhost:8080"
+	fmt.Println("Serever starting listening at", addr, "/ws")
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
